52b6c106b0bf2926688d302c393609b8cd7c98a7
ManhDevFull <thanhmanhdangfa@gmail.com>
2026-03-15 15:21:00 +0700

    update auth after reload web

--- lib/store.ts
+++ lib/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 import type { User } from "./types";
 
 type AuthState = {
@@ -8,9 +9,18 @@ type AuthState = {
   clearAuth: () => void;
 };
 
-export const useAuthStore = create<AuthState>()((set) => ({
-  token: null,
-  user: null,
-  setAuth: (token, user) => set({ token, user }),
-  clearAuth: () => set({ token: null, user: null })
-}));
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      token: null,
+      user: null,
+      setAuth: (token, user) => set({ token, user }),
+      clearAuth: () => set({ token: null, user: null })
+    }),
+    {
+      name: "truyenm_auth",
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({ token: state.token, user: state.user })
+    }
+  )
+);
